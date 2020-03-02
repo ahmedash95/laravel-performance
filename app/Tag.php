61cba1ede1a451441a2a29b55bda5d88d4c0c099
Ahmadgoma <agomaahu@gmail.com>
2020-03-02 23:18:11 +0200

    remove post table from belongs to many method due to laravel understand relation from table naming

--- app/Tag.php
+++ app/Tag.php
@@ -9,6 +9,6 @@ class Tag extends Model
     protected $guarded = [];
 
     public function posts(){
-        return $this->belongsToMany(Post::class,'post_tag');
+        return $this->belongsToMany(Post::class);
     }
 }
